042649213f8d6a0e3180b82a3ff64bf71db0697d
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-10 20:04:09 -0500

    Add cat dto

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
+  }
+
   @Get()
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
   }
 }
