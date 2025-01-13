41e788c4e9be6385e60d8cff08c85b0e50d343bb
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-13 16:04:14 -0500

    Use config service to inject proper env variable

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      global: true,
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '15s' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [UsersModule],
+  exports: [AuthService],
 })
 export class AuthModule {}
