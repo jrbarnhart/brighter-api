c2cca8450b2baf92c229bac3a4a2ef6ee15c250f
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-13 20:51:40 -0500

    Catch and handle prisma errors when creating region

--- src/regions/regions.service.ts
+++ src/regions/regions.service.ts
@@ -1,19 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { Prisma, Region } from '@prisma/client';
 import { CreateRegionDto } from './dto/create-region.dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class RegionsService {
   constructor(private prisma: PrismaService) {}
 
-  create(data: CreateRegionDto): Promise<Region> {
-    return this.prisma.region.create({
-      data: {
-        name: data.name,
-        rooms: { connect: data.roomIds.map((roomId) => ({ id: roomId })) },
-      },
-    });
+  async create(data: CreateRegionDto): Promise<Region> {
+    try {
+      return await this.prisma.region.create({
+        data: {
+          name: data.name,
+          rooms: { connect: data.roomIds.map((roomId) => ({ id: roomId })) },
+        },
+      });
+    } catch (error) {
+      // Handle specific Prisma error for unique constraints
+      if (error instanceof PrismaClientKnownRequestError) {
+        throw new BadRequestException('Validation failed');
+      }
+
+      // For any other error that we didn't explicitly handle
+      throw new BadRequestException('An unknown error occured');
+    }
   }
 
   findAll() {
