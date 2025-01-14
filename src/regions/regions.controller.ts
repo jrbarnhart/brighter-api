c654219b24a8f6b4bfc8f96d830c5af9e954d63d
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-13 20:36:09 -0500

    Make region names unique

--- src/regions/regions.controller.ts
+++ src/regions/regions.controller.ts
@@ -7,16 +7,19 @@ import {
   Param,
   Delete,
   UsePipes,
+  UseGuards,
 } from '@nestjs/common';
 import { RegionsService } from './regions.service';
 import { ZodValidationPipe } from 'src/validation/zodValidation.pipe';
 import { createRegionSchema, CreateRegionDto } from './dto/create-region.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('regions')
 export class RegionsController {
   constructor(private readonly regionsService: RegionsService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   @UsePipes(new ZodValidationPipe(createRegionSchema))
   create(@Body() createRegionDto: CreateRegionDto) {
     return this.regionsService.create(createRegionDto);
