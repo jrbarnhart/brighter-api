971800410e883edc37f55cce392b84f1ed2542f9
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-14 23:15:45 -0500

    Add placeholder handlers for npcs & vendors

--- src/npcs/npcs.controller.ts
+++ src/npcs/npcs.controller.ts
@@ -1,34 +1,74 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { NpcsService } from './npcs.service';
 import { CreateNpcDto } from './dto/create-npc.dto';
 import { UpdateNpcDto } from './dto/update-npc.dto';
+import { CreateVendorDto } from './dto/create-vendor.dto';
+import { UpdateVendorDto } from './dto/update-vendor.dto';
 
 @Controller('npcs')
 export class NpcsController {
   constructor(private readonly npcsService: NpcsService) {}
 
+  // Npcs
   @Post()
-  create(@Body() createNpcDto: CreateNpcDto) {
-    return this.npcsService.create(createNpcDto);
+  createNpc(@Body() createNpcDto: CreateNpcDto) {
+    return this.npcsService.createNpc(createNpcDto);
   }
 
   @Get()
-  findAll() {
-    return this.npcsService.findAll();
+  findAllNpcs() {
+    return this.npcsService.findAllNpcs();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.npcsService.findOne(+id);
+  findOneNpc(@Param('id') id: string) {
+    return this.npcsService.findOneNpc(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateNpcDto: UpdateNpcDto) {
-    return this.npcsService.update(+id, updateNpcDto);
+  updateNpc(@Param('id') id: string, @Body() updateNpcDto: UpdateNpcDto) {
+    return this.npcsService.updateNpc(+id, updateNpcDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.npcsService.remove(+id);
+  removeNpc(@Param('id') id: string) {
+    return this.npcsService.removeNpc(+id);
+  }
+
+  // Vendors
+  @Post('vendors')
+  createVendor(@Body() createVendorDto: CreateVendorDto) {
+    return this.npcsService.createVendor(createVendorDto);
+  }
+
+  @Get('vendors')
+  findAllVendors() {
+    return this.npcsService.findAllVendors();
+  }
+
+  @Get('vendors/:id')
+  findOneVendor(@Param('id') id: string) {
+    return this.npcsService.findOneVendor(+id);
+  }
+
+  @Patch('vendors/:id')
+  updateVendor(
+    @Param('id') id: string,
+    @Body() updateVendorDto: UpdateVendorDto,
+  ) {
+    return this.npcsService.updateVendor(+id, updateVendorDto);
+  }
+
+  @Delete('vendors/:id')
+  removeVendor(@Param('id') id: string) {
+    return this.npcsService.removeVendor(+id);
   }
 }
--- src/npcs/npcs.service.ts
+++ src/npcs/npcs.service.ts
@@ -1,26 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { CreateNpcDto } from './dto/create-npc.dto';
 import { UpdateNpcDto } from './dto/update-npc.dto';
+import { CreateVendorDto } from './dto/create-vendor.dto';
+import { UpdateVendorDto } from './dto/update-vendor.dto';
 
 @Injectable()
 export class NpcsService {
-  create(createNpcDto: CreateNpcDto) {
+  // Npcs
+  createNpc(createNpcDto: CreateNpcDto) {
     return 'This action adds a new npc';
   }
 
-  findAll() {
+  findAllNpcs() {
     return `This action returns all npcs`;
   }
 
-  findOne(id: number) {
+  findOneNpc(id: number) {
     return `This action returns a #${id} npc`;
   }
 
-  update(id: number, updateNpcDto: UpdateNpcDto) {
+  updateNpc(id: number, updateNpcDto: UpdateNpcDto) {
     return `This action updates a #${id} npc`;
   }
 
-  remove(id: number) {
+  removeNpc(id: number) {
     return `This action removes a #${id} npc`;
+  }
+
+  // Vendors
+  createVendor(createVendorDto: CreateVendorDto) {
+    return 'This action adds a new vendor';
+  }
+
+  findAllVendors() {
+    return `This action returns all vendors`;
+  }
+
+  findOneVendor(id: number) {
+    return `This action returns a #${id} vendor`;
+  }
+
+  updateVendor(id: number, updateVendorDto: UpdateVendorDto) {
+    return `This action updates a #${id} vendor`;
+  }
+
+  removeVendor(id: number) {
+    return `This action removes a #${id} vendor`;
   }
 }
