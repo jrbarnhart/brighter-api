452364a6e1fcd2ef23d9fdae0f525c857abe55b0
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-15 16:06:46 -0500

    Add placeholder handlers for skills and requirements

--- src/skills/skills.controller.ts
+++ src/skills/skills.controller.ts
@@ -1,34 +1,237 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { SkillsService } from './skills.service';
-import { CreateSkillDto } from './dto/create-skill.dto';
-import { UpdateSkillDto } from './dto/update-skill.dto';
+import { CreateCombatSkillDto } from './dto/combatSkill/create-combat-skill.dto';
+import { UpdateCombatSkillDto } from './dto/combatSkill/update-combat-skill.dto';
+import { CreateCombatSkillRequirementDto } from './dto/combatSkill/create-combat-skill-requirement.dto';
+import { UpdateCombatSkillRequirementDto } from './dto/combatSkill/update-combat-skill-requirement.dto';
+import { CreateGatheringSkillDto } from './dto/gatheringSkill/create-gathering-skill.dto';
+import { UpdateGatheringSkillDto } from './dto/gatheringSkill/update-gathering-skill.dto';
+import { CreateGatheringSkillRequirementDto } from './dto/gatheringSkill/create-gathering-skill-requirement.dto';
+import { UpdateGatheringSkillRequirementDto } from './dto/gatheringSkill/update-gathering-skill-requirement.dto';
+import { CreateCraftingSkillDto } from './dto/craftingSkill/create-crafting-skill.dto';
+import { UpdateCraftingSkillDto } from './dto/craftingSkill/update-crafting-skill.dto';
+import { CreateCraftingSkillRequirementDto } from './dto/craftingSkill/create-crafting-skill-requirement.dto';
+import { UpdateCraftingSkillRequirementDto } from './dto/craftingSkill/update-crafting-skill-requirement.dto';
 
 @Controller('skills')
 export class SkillsController {
   constructor(private readonly skillsService: SkillsService) {}
 
-  @Post()
-  create(@Body() createSkillDto: CreateSkillDto) {
-    return this.skillsService.create(createSkillDto);
-  }
-
+  // All skills
   @Get()
-  findAll() {
-    return this.skillsService.findAll();
+  findAllSkills() {
+    return this.skillsService.findAllSkills();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.skillsService.findOne(+id);
+  // Combat Skills
+  @Post('combat')
+  createCombatSkill(@Body() createCombatSkillDto: CreateCombatSkillDto) {
+    return this.skillsService.createCombatSkill(createCombatSkillDto);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSkillDto: UpdateSkillDto) {
-    return this.skillsService.update(+id, updateSkillDto);
+  @Get('combat')
+  findAllCombatSkills() {
+    return this.skillsService.findAllCombatSkills();
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.skillsService.remove(+id);
+  @Get('combat/:id')
+  findOneCombatSkill(@Param('id') id: string) {
+    return this.skillsService.findOneCombatSkill(+id);
+  }
+
+  @Patch('combat/:id')
+  updateCombatSkill(
+    @Param('id') id: string,
+    @Body() updateCombatSkillDto: UpdateCombatSkillDto,
+  ) {
+    return this.skillsService.updateCombatSkill(+id, updateCombatSkillDto);
+  }
+
+  @Delete('combat/:id')
+  removeCombatSkill(@Param('id') id: string) {
+    return this.skillsService.removeCombatSkill(+id);
+  }
+
+  // Combat Skill Requirements
+  @Post('combat/requirements')
+  createCombatSkillRequirement(
+    @Body() createCombatSkillRequirementDto: CreateCombatSkillRequirementDto,
+  ) {
+    return this.skillsService.createCombatSkillRequirement(
+      createCombatSkillRequirementDto,
+    );
+  }
+
+  @Get('combat/requirements')
+  findAllCombatSkillRequirements() {
+    return this.skillsService.findAllCombatSkillRequirements();
+  }
+
+  @Get('combat/requirements/:id')
+  findOneCombatSkillRequirement(@Param('id') id: string) {
+    return this.skillsService.findOneCombatSkillRequirement(+id);
+  }
+
+  @Patch('combat/requirements/:id')
+  updateCombatSkillRequirement(
+    @Param('id') id: string,
+    @Body() updateCombatSkillRequirementDto: UpdateCombatSkillRequirementDto,
+  ) {
+    return this.skillsService.updateCombatSkillRequirement(
+      +id,
+      updateCombatSkillRequirementDto,
+    );
+  }
+
+  @Delete('combat/requirements/:id')
+  removeCombatSkillRequirement(@Param('id') id: string) {
+    return this.skillsService.removeCombatSkillRequirement(+id);
+  }
+
+  // Gathering Skills
+  @Post('gathering')
+  createGatheringSkill(
+    @Body() createGatheringSkillDto: CreateGatheringSkillDto,
+  ) {
+    return this.skillsService.createGatheringSkill(createGatheringSkillDto);
+  }
+
+  @Get('gathering')
+  findAllGatheringSkills() {
+    return this.skillsService.findAllGatheringSkills();
+  }
+
+  @Get('gathering/:id')
+  findOneGatheringSkill(@Param('id') id: string) {
+    return this.skillsService.findOneGatheringSkill(+id);
+  }
+
+  @Patch('gathering/:id')
+  updateGatheringSkill(
+    @Param('id') id: string,
+    @Body() updateGatheringSkillDto: UpdateGatheringSkillDto,
+  ) {
+    return this.skillsService.updateGatheringSkill(
+      +id,
+      updateGatheringSkillDto,
+    );
+  }
+
+  @Delete('gathering/:id')
+  removeGatheringSkill(@Param('id') id: string) {
+    return this.skillsService.removeGatheringSkill(+id);
+  }
+
+  // Gathering Skill Requirements
+  @Post('gathering/requirements')
+  createGatheringSkillRequirement(
+    @Body()
+    createGatheringSkillRequirementDto: CreateGatheringSkillRequirementDto,
+  ) {
+    return this.skillsService.createGatheringSkillRequirement(
+      createGatheringSkillRequirementDto,
+    );
+  }
+
+  @Get('gathering/requirements')
+  findAllGatheringSkillRequirements() {
+    return this.skillsService.findAllGatheringSkillRequirements();
+  }
+
+  @Get('gathering/requirements/:id')
+  findOneGatheringSkillRequirement(@Param('id') id: string) {
+    return this.skillsService.findOneGatheringSkillRequirement(+id);
+  }
+
+  @Patch('gathering/requirements/:id')
+  updateGatheringSkillRequirement(
+    @Param('id') id: string,
+    @Body()
+    updateGatheringSkillRequirementDto: UpdateGatheringSkillRequirementDto,
+  ) {
+    return this.skillsService.updateGatheringSkillRequirement(
+      +id,
+      updateGatheringSkillRequirementDto,
+    );
+  }
+
+  @Delete('gathering/requirements/:id')
+  removeGatheringSkillRequirement(@Param('id') id: string) {
+    return this.skillsService.removeGatheringSkillRequirement(+id);
+  }
+
+  // Crafting Skills
+  @Post('crafting')
+  createCraftingSkill(@Body() createCraftingSkillDto: CreateCraftingSkillDto) {
+    return this.skillsService.createCraftingSkill(createCraftingSkillDto);
+  }
+
+  @Get('crafting')
+  findAllCraftingSkills() {
+    return this.skillsService.findAllCraftingSkills();
+  }
+
+  @Get('crafting/:id')
+  findOneCraftingSkill(@Param('id') id: string) {
+    return this.skillsService.findOneCraftingSkill(+id);
+  }
+
+  @Patch('crafting/:id')
+  updateCraftingSkill(
+    @Param('id') id: string,
+    @Body() updateCraftingSkillDto: UpdateCraftingSkillDto,
+  ) {
+    return this.skillsService.updateCraftingSkill(+id, updateCraftingSkillDto);
+  }
+
+  @Delete('crafting/:id')
+  removeCraftingSkill(@Param('id') id: string) {
+    return this.skillsService.removeCraftingSkill(+id);
+  }
+
+  // Crafting Skill Requirements
+  @Post('crafting/requirements')
+  createCraftingSkillRequirement(
+    @Body()
+    createCraftingSkillRequirementDto: CreateCraftingSkillRequirementDto,
+  ) {
+    return this.skillsService.createCraftingSkillRequirement(
+      createCraftingSkillRequirementDto,
+    );
+  }
+
+  @Get('crafting/requirements')
+  findAllCraftingSkillRequirements() {
+    return this.skillsService.findAllCraftingSkillRequirements();
+  }
+
+  @Get('crafting/requirements/:id')
+  findOneCraftingSkillRequirement(@Param('id') id: string) {
+    return this.skillsService.findOneCraftingSkillRequirement(+id);
+  }
+
+  @Patch('crafting/requirements/:id')
+  updateCraftingSkillRequirement(
+    @Param('id') id: string,
+    @Body()
+    updateCraftingSkillRequirementDto: UpdateCraftingSkillRequirementDto,
+  ) {
+    return this.skillsService.updateCraftingSkillRequirement(
+      +id,
+      updateCraftingSkillRequirementDto,
+    );
+  }
+
+  @Delete('crafting/requirements/:id')
+  removeCraftingSkillRequirement(@Param('id') id: string) {
+    return this.skillsService.removeCraftingSkillRequirement(+id);
   }
 }
--- src/skills/skills.service.ts
+++ src/skills/skills.service.ts
@@ -1,26 +1,169 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
-import { CreateSkillDto } from './dto/create-skill.dto';
-import { UpdateSkillDto } from './dto/update-skill.dto';
+import { CreateCombatSkillDto } from './dto/combatSkill/create-combat-skill.dto';
+import { UpdateCombatSkillDto } from './dto/combatSkill/update-combat-skill.dto';
+import { CreateCombatSkillRequirementDto } from './dto/combatSkill/create-combat-skill-requirement.dto';
+import { UpdateCombatSkillRequirementDto } from './dto/combatSkill/update-combat-skill-requirement.dto';
+import { CreateGatheringSkillDto } from './dto/gatheringSkill/create-gathering-skill.dto';
+import { UpdateGatheringSkillDto } from './dto/gatheringSkill/update-gathering-skill.dto';
+import { CreateGatheringSkillRequirementDto } from './dto/gatheringSkill/create-gathering-skill-requirement.dto';
+import { UpdateGatheringSkillRequirementDto } from './dto/gatheringSkill/update-gathering-skill-requirement.dto';
+import { CreateCraftingSkillDto } from './dto/craftingSkill/create-crafting-skill.dto';
+import { UpdateCraftingSkillDto } from './dto/craftingSkill/update-crafting-skill.dto';
+import { CreateCraftingSkillRequirementDto } from './dto/craftingSkill/create-crafting-skill-requirement.dto';
+import { UpdateCraftingSkillRequirementDto } from './dto/craftingSkill/update-crafting-skill-requirement.dto';
 
 @Injectable()
 export class SkillsService {
-  create(createSkillDto: CreateSkillDto) {
-    return 'This action adds a new skill';
-  }
-
-  findAll() {
+  //Skills
+  findAllSkills() {
     return `This action returns all skills`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} skill`;
+  // Combat Skills
+  createCombatSkill(createCombatSkillDto: CreateCombatSkillDto) {
+    return 'This action adds a new combat skill';
   }
 
-  update(id: number, updateSkillDto: UpdateSkillDto) {
-    return `This action updates a #${id} skill`;
+  findAllCombatSkills() {
+    return `This action returns all combat skills`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} skill`;
+  findOneCombatSkill(id: number) {
+    return `This action returns a #${id} combat skill`;
+  }
+
+  updateCombatSkill(id: number, updateCombatSkillDto: UpdateCombatSkillDto) {
+    return `This action updates a #${id} combat skill`;
+  }
+
+  removeCombatSkill(id: number) {
+    return `This action removes a #${id} combat skill`;
+  }
+
+  // Combat Skill Requirements
+  createCombatSkillRequirement(
+    createCombatSkillRequirementDto: CreateCombatSkillRequirementDto,
+  ) {
+    return 'This action adds a new combat skill requirement';
+  }
+
+  findAllCombatSkillRequirements() {
+    return `This action returns all combat skill requirements`;
+  }
+
+  findOneCombatSkillRequirement(id: number) {
+    return `This action returns a #${id} combat skill requirement`;
+  }
+
+  updateCombatSkillRequirement(
+    id: number,
+    updateCombatSkillRequirementDto: UpdateCombatSkillRequirementDto,
+  ) {
+    return `This action updates a #${id} combat skill requirement`;
+  }
+
+  removeCombatSkillRequirement(id: number) {
+    return `This action removes a #${id} combat skill requirement`;
+  }
+
+  // Gathering Skills
+  createGatheringSkill(createGatheringSkillDto: CreateGatheringSkillDto) {
+    return 'This action adds a new gathering skill';
+  }
+
+  findAllGatheringSkills() {
+    return `This action returns all gathering skills`;
+  }
+
+  findOneGatheringSkill(id: number) {
+    return `This action returns a #${id} gathering skill`;
+  }
+
+  updateGatheringSkill(
+    id: number,
+    updateGatheringSkillDto: UpdateGatheringSkillDto,
+  ) {
+    return `This action updates a #${id} gathering skill`;
+  }
+
+  removeGatheringSkill(id: number) {
+    return `This action removes a #${id} gathering skill`;
+  }
+
+  // Gathering Skill Requirements
+  createGatheringSkillRequirement(
+    createGatheringSkillRequirementDto: CreateGatheringSkillRequirementDto,
+  ) {
+    return 'This action adds a new gathering skill requirement';
+  }
+
+  findAllGatheringSkillRequirements() {
+    return `This action returns all gathering skill requirements`;
+  }
+
+  findOneGatheringSkillRequirement(id: number) {
+    return `This action returns a #${id} gathering skill requirement`;
+  }
+
+  updateGatheringSkillRequirement(
+    id: number,
+    updateGatheringSkillRequirementDto: UpdateGatheringSkillRequirementDto,
+  ) {
+    return `This action updates a #${id} gathering skill requirement`;
+  }
+
+  removeGatheringSkillRequirement(id: number) {
+    return `This action removes a #${id} gathering skill requirement`;
+  }
+
+  // Crafting Skills
+  createCraftingSkill(createCraftingSkillDto: CreateCraftingSkillDto) {
+    return 'This action adds a new crafting skill';
+  }
+
+  findAllCraftingSkills() {
+    return `This action returns all crafting skills`;
+  }
+
+  findOneCraftingSkill(id: number) {
+    return `This action returns a #${id} crafting skill`;
+  }
+
+  updateCraftingSkill(
+    id: number,
+    updateCraftingSkillDto: UpdateCraftingSkillDto,
+  ) {
+    return `This action updates a #${id} crafting skill`;
+  }
+
+  removeCraftingSkill(id: number) {
+    return `This action removes a #${id} crafting skill`;
+  }
+
+  // Crafting Skill Requirements
+  createCraftingSkillRequirement(
+    createCraftingSkillRequirementDto: CreateCraftingSkillRequirementDto,
+  ) {
+    return 'This action adds a new crafting skill requirement';
+  }
+
+  findAllCraftingSkillRequirements() {
+    return `This action returns all crafting skill requirements`;
+  }
+
+  findOneCraftingSkillRequirement(id: number) {
+    return `This action returns a #${id} crafting skill requirement`;
+  }
+
+  updateCraftingSkillRequirement(
+    id: number,
+    updateCraftingSkillRequirementDto: UpdateCraftingSkillRequirementDto,
+  ) {
+    return `This action updates a #${id} crafting skill requirement`;
+  }
+
+  removeCraftingSkillRequirement(id: number) {
+    return `This action removes a #${id} crafting skill requirement`;
   }
 }
