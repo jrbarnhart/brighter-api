4dbbebde45cb9664d3c812b7a429333a0ca99aa5
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-14 21:02:49 -0500

    Add basic services and handlers for resource variants

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -12,31 +12,66 @@ import { ItemsService } from './items.service';
 import { CreateResourceDto } from './dto/resource/create-resource.dto';
 import { UpdateResourceDto } from './dto/resource/update-resource.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { CreateResourceVariantDto } from './dto/resource/create-resource-variant.dto';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
+  // Resource Variants
+  @Post('resources/variants')
+  @UseGuards(AuthGuard)
+  createResourceVariant(
+    @Body() createResourceVariantDto: CreateResourceVariantDto,
+  ) {
+    return this.itemsService.createResourceVariant(createResourceVariantDto);
+  }
+
+  @Get('resources/variants')
+  findAllResourceVariants() {
+    return this.itemsService.findAllResourceVariants();
+  }
+
+  @Get('resources/variants/:id')
+  findOneResourceVariant(@Param('id') id: string) {
+    return this.itemsService.findOneResourceVariant(+id);
+  }
+
+  @Patch('resources/variants/:id')
+  @UseGuards(AuthGuard)
+  updateResourceVariant(
+    @Param('id') id: string,
+    @Body() updateResourceDto: UpdateResourceDto,
+  ) {
+    return this.itemsService.updateResourceVariant(+id, updateResourceDto);
+  }
+
+  @Delete('resources/variants/:id')
+  @UseGuards(AuthGuard)
+  removeResourceVariant(@Param('id') id: string) {
+    return this.itemsService.removeResourceVariant(+id);
+  }
+
   // Resources
   @Post('resources')
   @UseGuards(AuthGuard)
-  create(@Body() createItemDto: CreateResourceDto) {
-    return this.itemsService.createResource(createItemDto);
+  createResource(@Body() createResourceDto: CreateResourceDto) {
+    return this.itemsService.createResource(createResourceDto);
   }
 
   @Get('resources')
-  findAll() {
+  findAllResources() {
     return this.itemsService.findAllResources();
   }
 
   @Get('resources/:id')
-  findOne(@Param('id') id: string) {
+  findOneResource(@Param('id') id: string) {
     return this.itemsService.findOneResource(+id);
   }
 
   @Patch('resources/:id')
   @UseGuards(AuthGuard)
-  update(
+  updateResource(
     @Param('id') id: string,
     @Body() updateResourceDto: UpdateResourceDto,
   ) {
@@ -45,13 +80,19 @@ export class ItemsController {
 
   @Delete('resources/:id')
   @UseGuards(AuthGuard)
-  remove(@Param('id') id: string) {
+  removeResource(@Param('id') id: string) {
     return this.itemsService.removeResource(+id);
   }
 
+  // Consumable Variants
+
   // Consumables
 
+  // Weapon Variants
+
   // Weapons
+
+  // Armor Variants
 
   // Armor
 
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,33 +1,65 @@
 import { Injectable } from '@nestjs/common';
 import { CreateResourceDto } from './dto/resource/create-resource.dto';
 import { UpdateResourceDto } from './dto/resource/update-resource.dto';
+import { CreateResourceVariantDto } from './dto/resource/create-resource-variant.dto';
+import { UpdateResourceVariantDto } from './dto/resource/update-resource-variant.dto';
 
 @Injectable()
 export class ItemsService {
   // Resources
   createResource(createResourceDto: CreateResourceDto) {
-    return 'This action adds a new item';
+    return 'This action adds a new resource';
   }
 
   findAllResources() {
-    return `This action returns all items`;
+    return `This action returns all resources`;
   }
 
   findOneResource(id: number) {
-    return `This action returns a #${id} item`;
+    return `This action returns a #${id} resource`;
   }
 
   updateResource(id: number, updateResourceDto: UpdateResourceDto) {
-    return `This action updates a #${id} item`;
+    return `This action updates a #${id} resource`;
   }
 
   removeResource(id: number) {
-    return `This action removes a #${id} item`;
+    return `This action removes a #${id} resource`;
   }
+
+  // Resource Variants
+  createResourceVariant(createResourceVariantDto: CreateResourceVariantDto) {
+    return 'This action adds a new resource variant';
+  }
+
+  findAllResourceVariants() {
+    return `This action returns all resource variants`;
+  }
+
+  findOneResourceVariant(id: number) {
+    return `This action returns a #${id} resource variant`;
+  }
+
+  updateResourceVariant(
+    id: number,
+    updateResourceVariantDto: UpdateResourceVariantDto,
+  ) {
+    return `This action updates a #${id} resource variant`;
+  }
+
+  removeResourceVariant(id: number) {
+    return `This action removes a #${id} resource variant`;
+  }
+
+  // Consumable Variants
 
   // Consumables
 
+  // Weapon Variants
+
   // Weapons
+
+  // Armor Variants
 
   // Armor
 
