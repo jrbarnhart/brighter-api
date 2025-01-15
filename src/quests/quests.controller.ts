ee990e0c2d56f78bfd5ca6fe81417e7bc5c5f5d5
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-14 23:29:19 -0500

    Add placeholder handlers for quests & quest steps

--- src/quests/quests.controller.ts
+++ src/quests/quests.controller.ts
@@ -1,34 +1,74 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { QuestsService } from './quests.service';
 import { CreateQuestDto } from './dto/create-quest.dto';
 import { UpdateQuestDto } from './dto/update-quest.dto';
+import { CreateQuestStepDto } from './dto/create-quest-step.dto';
+import { UpdateQuestStepDto } from './dto/update-quest-step.dto';
 
 @Controller('quests')
 export class QuestsController {
   constructor(private readonly questsService: QuestsService) {}
 
+  // Quests
   @Post()
-  create(@Body() createQuestDto: CreateQuestDto) {
-    return this.questsService.create(createQuestDto);
+  createQuest(@Body() createQuestDto: CreateQuestDto) {
+    return this.questsService.createQuest(createQuestDto);
   }
 
   @Get()
-  findAll() {
-    return this.questsService.findAll();
+  findAllQuests() {
+    return this.questsService.findAllQuests();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.questsService.findOne(+id);
+  findOneQuest(@Param('id') id: string) {
+    return this.questsService.findOneQuest(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateQuestDto: UpdateQuestDto) {
-    return this.questsService.update(+id, updateQuestDto);
+  updateQuest(@Param('id') id: string, @Body() updateQuestDto: UpdateQuestDto) {
+    return this.questsService.updateQuest(+id, updateQuestDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.questsService.remove(+id);
+  removeQuest(@Param('id') id: string) {
+    return this.questsService.removeQuest(+id);
+  }
+
+  // Quest Steps
+  @Post()
+  createQuestStep(@Body() createQuestStepDto: CreateQuestStepDto) {
+    return this.questsService.createQuestStep(createQuestStepDto);
+  }
+
+  @Get()
+  findAllQuestsStep() {
+    return this.questsService.findAllQuestSteps();
+  }
+
+  @Get(':id')
+  findOneQuestStep(@Param('id') id: string) {
+    return this.questsService.findOneQuestStep(+id);
+  }
+
+  @Patch(':id')
+  updateQuestStep(
+    @Param('id') id: string,
+    @Body() updateQuestStepDto: UpdateQuestStepDto,
+  ) {
+    return this.questsService.updateQuestStep(+id, updateQuestStepDto);
+  }
+
+  @Delete(':id')
+  removeQuestStep(@Param('id') id: string) {
+    return this.questsService.removeQuestStep(+id);
   }
 }
--- src/quests/quests.service.ts
+++ src/quests/quests.service.ts
@@ -1,26 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { CreateQuestDto } from './dto/create-quest.dto';
 import { UpdateQuestDto } from './dto/update-quest.dto';
+import { CreateQuestStepDto } from './dto/create-quest-step.dto';
+import { UpdateQuestStepDto } from './dto/update-quest-step.dto';
 
 @Injectable()
 export class QuestsService {
-  create(createQuestDto: CreateQuestDto) {
+  // Quests
+  createQuest(createQuestDto: CreateQuestDto) {
     return 'This action adds a new quest';
   }
 
-  findAll() {
+  findAllQuests() {
     return `This action returns all quests`;
   }
 
-  findOne(id: number) {
+  findOneQuest(id: number) {
     return `This action returns a #${id} quest`;
   }
 
-  update(id: number, updateQuestDto: UpdateQuestDto) {
+  updateQuest(id: number, updateQuestDto: UpdateQuestDto) {
     return `This action updates a #${id} quest`;
   }
 
-  remove(id: number) {
+  removeQuest(id: number) {
     return `This action removes a #${id} quest`;
+  }
+
+  // Quest Steps
+  createQuestStep(createQuestStepDto: CreateQuestStepDto) {
+    return 'This action adds a new quest step';
+  }
+
+  findAllQuestSteps() {
+    return `This action returns all quest steps`;
+  }
+
+  findOneQuestStep(id: number) {
+    return `This action returns a #${id} quest step`;
+  }
+
+  updateQuestStep(id: number, updateQuestStepDto: UpdateQuestStepDto) {
+    return `This action updates a #${id} quest step`;
+  }
+
+  removeQuestStep(id: number) {
+    return `This action removes a #${id} quest step`;
   }
 }
