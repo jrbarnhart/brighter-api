eb363aefdb4f23238137acae01ee79464f28139c
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-15 16:52:33 -0500

    Require at least one property for updates

--- src/regions/dto/update-region.dto.ts
+++ src/regions/dto/update-region.dto.ts
@@ -1,7 +1,11 @@
 import { z } from 'zod';
 
-export const updateRegionSchema = z.object({
-  name: z.string().optional(),
-});
+export const updateRegionSchema = z
+  .object({
+    name: z.string().optional(),
+  })
+  .refine((obj) => Object.keys(obj).length > 0, {
+    message: 'At least one property is required in body',
+  });
 
 export type UpdateRegionDto = z.infer<typeof updateRegionSchema>;
--- src/rooms/dto/update-room.dto.ts
+++ src/rooms/dto/update-room.dto.ts
@@ -1,16 +1,20 @@
 import { z } from 'zod';
 
-export const updateRoomSchema = z.object({
-  name: z.string().optional(),
-  regionId: z.number().int().positive().optional(),
-  craftingSpotIds: z.array(z.number().int().positive()).optional(),
-  monsterIds: z.array(z.number().int().positive()).optional(),
-  npcIds: z.array(z.number().int().positive()).optional(),
-  resourceIds: z.array(z.number().int().positive()).optional(),
-  questStepIds: z.array(z.number().int().positive()).optional(),
-  bankIds: z.array(z.number().int().positive()).optional(),
-  portal: z.boolean().optional(),
-  obelisk: z.boolean().optional(),
-});
+export const updateRoomSchema = z
+  .object({
+    name: z.string().optional(),
+    regionId: z.number().int().positive().optional(),
+    craftingSpotIds: z.array(z.number().int().positive()).optional(),
+    monsterIds: z.array(z.number().int().positive()).optional(),
+    npcIds: z.array(z.number().int().positive()).optional(),
+    resourceIds: z.array(z.number().int().positive()).optional(),
+    questStepIds: z.array(z.number().int().positive()).optional(),
+    bankIds: z.array(z.number().int().positive()).optional(),
+    portal: z.boolean().optional(),
+    obelisk: z.boolean().optional(),
+  })
+  .refine((obj) => Object.keys(obj).length > 0, {
+    message: 'At least one property is required in body',
+  });
 
 export type UpdateRoomDto = z.infer<typeof updateRoomSchema>;
