608ef275fcd78cf10e0840649bd6a9fbffad205a
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-14 19:53:15 -0500

    Remove cats and logger from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
-import { logger } from './middleware/logger.middleware';
-import { CatsController } from './cats/cats.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
@@ -11,7 +8,6 @@ import { RegionsModule } from './regions/regions.module';
 
 @Module({
   imports: [
-    CatsModule,
     AuthModule,
     UsersModule,
     ConfigModule.forRoot({ isGlobal: true }),
@@ -20,8 +16,4 @@ import { RegionsModule } from './regions/regions.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(logger).forRoutes(CatsController);
-  }
-}
+export class AppModule {}
