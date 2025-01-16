37c88d2e346327e78c5d2d313986f8fc78b1883d
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-15 21:00:05 -0500

    Add tests for findAll and findOne

--- src/regions/regions.controller.spec.ts
+++ src/regions/regions.controller.spec.ts
@@ -2,10 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { RegionsController } from './regions.controller';
 import { RegionsService } from './regions.service';
 import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Region } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { NotFoundException } from '@nestjs/common';
 
 describe('RegionsController', () => {
   let controller: RegionsController;
@@ -35,5 +36,39 @@ describe('RegionsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return an array of regions', async () => {
+      const allRegions: Region[] = [{ id: 1, name: 'Region One' }];
+
+      regionsServiceMock.findAll.mockResolvedValue(allRegions);
+
+      expect(await controller.findAll()).toBe(allRegions);
+    });
+
+    it('should return empty array when there are no regions', async () => {
+      const allRegions: Region[] = [];
+
+      regionsServiceMock.findAll.mockResolvedValue(allRegions);
+
+      expect(await controller.findAll()).toBe(allRegions);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return the region if it exists', async () => {
+      const region: Region = { id: 1, name: 'Region One' };
+
+      regionsServiceMock.findOne.mockResolvedValue(region);
+
+      expect(await controller.findOne(region.id)).toBe(region);
+    });
+
+    it('should return NotFound exception if region does not exist', async () => {
+      regionsServiceMock.findOne.mockRejectedValue(new NotFoundException());
+
+      await expect(controller.findOne(999)).rejects.toThrow(NotFoundException);
+    });
   });
 });
