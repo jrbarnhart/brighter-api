273e41815f5f042ef5ed080903df9855d30847a7
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-16 15:15:53 -0500

    Mock required services

--- src/items/items.controller.spec.ts
+++ src/items/items.controller.spec.ts
@@ -1,14 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { PrismaClient } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 describe('ItemsController', () => {
   let controller: ItemsController;
+  let prismaMock: DeepMockProxy<PrismaClient>;
+  let jwtServiceMock: DeepMockProxy<JwtService>;
+  let configServiceMock: DeepMockProxy<ConfigService>;
+  let itemsServiceMock: DeepMockProxy<ItemsService>;
 
   beforeEach(async () => {
+    prismaMock = mockDeep<PrismaClient>();
+    jwtServiceMock = mockDeep<JwtService>();
+    configServiceMock = mockDeep<ConfigService>();
+    itemsServiceMock = mockDeep<ItemsService>();
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ItemsController],
-      providers: [ItemsService],
+      providers: [
+        ItemsService,
+        { provide: PrismaService, useValue: prismaMock },
+        { provide: JwtService, useValue: jwtServiceMock },
+        { provide: ConfigService, useValue: configServiceMock },
+        { provide: ItemsService, useValue: itemsServiceMock },
+      ],
     }).compile();
 
     controller = module.get<ItemsController>(ItemsController);
