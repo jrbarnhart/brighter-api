672faacdb4f452603a7b58bde8970704a2e552a5
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-16 15:27:00 -0500

    Add tests for get resource variant by id

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -3,6 +3,7 @@ import { ItemsService } from './items.service';
 import { Prisma, PrismaClient, ResourceVariant } from '@prisma/client';
 import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
 import { PrismaService } from 'src/prisma.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('ItemsService', () => {
   let service: ItemsService;
@@ -72,6 +73,69 @@ describe('ItemsService', () => {
         expect(prismaMock.resourceVariant.findMany).toHaveBeenCalledWith(
           findManyArgsMock,
         );
+      });
+    });
+
+    describe('getResourceVariantById', () => {
+      describe('getResourceVariantById', () => {
+        it('should return the resourceVariant if it exists', async () => {
+          const existingResourceVariant: ResourceVariant = {
+            id: 1,
+            name: 'ResourceVariant One',
+            requirementId: 10,
+            resourceId: 100,
+          };
+          const findUniqueArgsMock: Prisma.ResourceVariantFindUniqueArgs = {
+            where: { id: existingResourceVariant.id },
+            include: {
+              resource: true,
+              requirement: true,
+              dropTables: true,
+              inRecipes: true,
+              vendors: true,
+            },
+          };
+
+          prismaMock.resourceVariant.findUnique.mockResolvedValue(
+            existingResourceVariant,
+          );
+
+          const result = await service.findOneResourceVariant(
+            existingResourceVariant.id,
+          );
+          expect(result).toEqual(existingResourceVariant);
+          expect(prismaMock.resourceVariant.findUnique).toHaveBeenCalledTimes(
+            1,
+          );
+          expect(prismaMock.resourceVariant.findUnique).toHaveBeenCalledWith(
+            findUniqueArgsMock,
+          );
+        });
+
+        it('should throw NotFoundException if resourceVariant does not exist', async () => {
+          const findUniqueArgsMock: Prisma.ResourceVariantFindUniqueArgs = {
+            where: { id: 999 },
+            include: {
+              resource: true,
+              requirement: true,
+              dropTables: true,
+              inRecipes: true,
+              vendors: true,
+            },
+          };
+
+          prismaMock.resourceVariant.findUnique.mockResolvedValue(null);
+
+          await expect(service.findOneResourceVariant(999)).rejects.toThrow(
+            NotFoundException,
+          );
+          expect(prismaMock.resourceVariant.findUnique).toHaveBeenCalledTimes(
+            1,
+          );
+          expect(prismaMock.resourceVariant.findUnique).toHaveBeenCalledWith(
+            findUniqueArgsMock,
+          );
+        });
       });
     });
   });
