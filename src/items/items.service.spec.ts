619a830c396ee59d8320e8e4509fce0768f006d8
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-16 15:19:31 -0500

    Add tests for get resource variants

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ItemsService } from './items.service';
+import { Prisma, PrismaClient, ResourceVariant } from '@prisma/client';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { PrismaService } from 'src/prisma.service';
 
 describe('ItemsService', () => {
   let service: ItemsService;
+  let prismaMock: DeepMockProxy<PrismaClient>;
 
   beforeEach(async () => {
+    prismaMock = mockDeep<PrismaClient>();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ItemsService],
+      providers: [
+        ItemsService,
+        { provide: PrismaService, useValue: prismaMock },
+      ],
     }).compile();
 
     service = module.get<ItemsService>(ItemsService);
@@ -14,5 +23,56 @@ describe('ItemsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Resource Variants', () => {
+    describe('getAllResourceVariants', () => {
+      it('should return all resource variants array', async () => {
+        const allResourceVariants: ResourceVariant[] = [
+          { id: 1, name: 'small', resourceId: 10, requirementId: null },
+        ];
+        const findManyArgsMock: Prisma.ResourceVariantFindManyArgs = {
+          include: {
+            resource: true,
+            requirement: true,
+            dropTables: true,
+            inRecipes: true,
+            vendors: true,
+          },
+        };
+
+        prismaMock.resourceVariant.findMany.mockResolvedValue(
+          allResourceVariants,
+        );
+
+        const result = await service.findAllResourceVariants();
+        expect(result).toBe(allResourceVariants);
+        expect(prismaMock.resourceVariant.findMany).toHaveBeenCalledTimes(1);
+        expect(prismaMock.resourceVariant.findMany).toHaveBeenCalledWith(
+          findManyArgsMock,
+        );
+      });
+
+      it('should return empty array if there are no resourceVariants', async () => {
+        const findManyArgsMock: Prisma.ResourceVariantFindManyArgs = {
+          include: {
+            resource: true,
+            requirement: true,
+            dropTables: true,
+            inRecipes: true,
+            vendors: true,
+          },
+        };
+
+        prismaMock.resourceVariant.findMany.mockResolvedValue([]);
+
+        const result = await service.findAllResourceVariants();
+        expect(result).toEqual([]);
+        expect(prismaMock.resourceVariant.findMany).toHaveBeenCalledTimes(1);
+        expect(prismaMock.resourceVariant.findMany).toHaveBeenCalledWith(
+          findManyArgsMock,
+        );
+      });
+    });
   });
 });
