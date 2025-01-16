73a72c79726ad7720683c696793fc878f6fb9400
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-15 21:29:45 -0500

    Add tests for removing

--- src/regions/regions.controller.spec.ts
+++ src/regions/regions.controller.spec.ts
@@ -6,7 +6,8 @@ import { PrismaClient, Region } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
-import { NotFoundException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+import { UpdateRegionDto } from './dto/update-region.dto';
 
 describe('RegionsController', () => {
   let controller: RegionsController;
@@ -65,10 +66,64 @@ describe('RegionsController', () => {
       expect(await controller.findOne(region.id)).toBe(region);
     });
 
-    it('should return NotFound exception if region does not exist', async () => {
+    it('should throw NotFoundException if region does not exist', async () => {
       regionsServiceMock.findOne.mockRejectedValue(new NotFoundException());
 
       await expect(controller.findOne(999)).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('create', () => {
+    it('should add a new region', async () => {
+      const region: Region = { id: 1, name: 'New Region' };
+
+      regionsServiceMock.create.mockResolvedValue(region);
+
+      expect(await controller.create(region)).toBe(region);
+    });
+  });
+
+  describe('update', () => {
+    it('should update the region if it exists', async () => {
+      const updateRegionDto: UpdateRegionDto = { name: 'Updated Region' };
+      const updatedRegion: Region = { id: 1, name: 'Updated Region' };
+
+      regionsServiceMock.update.mockResolvedValue(updatedRegion);
+
+      expect(await controller.update(updatedRegion.id, updateRegionDto)).toBe(
+        updatedRegion,
+      );
+    });
+
+    it('should throw a NotFoundException if the region does not exist', async () => {
+      const updateRegionDto: UpdateRegionDto = { name: 'Updated Region' };
+      regionsServiceMock.update.mockRejectedValue(new NotFoundException());
+
+      await expect(controller.update(999, updateRegionDto)).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete the region if it exists', async () => {
+      const region: Region = { id: 1, name: 'Updated Region' };
+
+      regionsServiceMock.remove.mockResolvedValue(region);
+
+      expect(await controller.remove(region.id)).toBe(region);
+    });
+
+    it('should throw a NotFoundException if the region does not exist', async () => {
+      regionsServiceMock.remove.mockRejectedValue(new NotFoundException());
+
+      await expect(controller.remove(999)).rejects.toThrow(NotFoundException);
+    });
+
+    it('should throw a BadRequestException if the region has rooms', async () => {
+      regionsServiceMock.remove.mockRejectedValue(new BadRequestException());
+
+      await expect(controller.remove(999)).rejects.toThrow(BadRequestException);
     });
   });
 });
