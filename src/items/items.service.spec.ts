876c2f95e2372c60e7c6c8fac26c37e1a749e9eb
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-16 17:05:55 -0500

    Add tests for weapons

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -7,6 +7,7 @@ import {
   PrismaClient,
   Resource,
   ResourceVariant,
+  Weapon,
   WeaponVariant,
 } from '@prisma/client';
 import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
@@ -22,6 +23,8 @@ import { CreateConsumableDto } from './dto/consumable/create-consumable.dto';
 import { UpdateConsumableDto } from './dto/consumable/update-consumable.dto';
 import { CreateWeaponVariantDto } from './dto/weapon/create-weapon-variant.dto';
 import { UpdateWeaponVariantDto } from './dto/weapon/update-weapon-variant.dto';
+import { CreateWeaponDto } from './dto/weapon/create-weapon.dto';
+import { UpdateWeaponDto } from './dto/weapon/update-weapon.dto';
 
 describe('ItemsService', () => {
   let service: ItemsService;
@@ -1203,7 +1206,239 @@ describe('ItemsService', () => {
     });
   });
 
-  describe('Weapons', () => {});
+  describe('Weapons', () => {
+    describe('getAllWeapons', () => {
+      it('should return all weapons array', async () => {
+        const allWeapons: Weapon[] = [
+          {
+            id: 1,
+            name: 'Weapon One',
+            element: 'IMPACT',
+            faction: 'NONE',
+            isRanged: false,
+            isTwoHanded: true,
+          },
+          {
+            id: 2,
+            name: 'Weapon Two',
+            element: 'IMPACT',
+            faction: 'NONE',
+            isRanged: false,
+            isTwoHanded: true,
+          },
+        ];
+        const findManyArgsMock: Prisma.WeaponFindManyArgs = {
+          include: {
+            variants: true,
+          },
+        };
+
+        prismaMock.weapon.findMany.mockResolvedValue(allWeapons);
+
+        const result = await service.findAllWeapons();
+        expect(result).toEqual(allWeapons);
+        expect(prismaMock.weapon.findMany).toHaveBeenCalledTimes(1);
+        expect(prismaMock.weapon.findMany).toHaveBeenCalledWith(
+          findManyArgsMock,
+        );
+      });
+
+      it('should return empty array if there are no weapons', async () => {
+        const findManyArgsMock: Prisma.WeaponFindManyArgs = {
+          include: {
+            variants: true,
+          },
+        };
+
+        prismaMock.weapon.findMany.mockResolvedValue([]);
+
+        const result = await service.findAllWeapons();
+        expect(result).toEqual([]);
+        expect(prismaMock.weapon.findMany).toHaveBeenCalledTimes(1);
+        expect(prismaMock.weapon.findMany).toHaveBeenCalledWith(
+          findManyArgsMock,
+        );
+      });
+    });
+
+    describe('getWeaponById', () => {
+      it('should return the weapon if it exists', async () => {
+        const existingWeapon: Weapon = {
+          id: 1,
+          name: 'Weapon One',
+          element: 'IMPACT',
+          faction: 'NONE',
+          isRanged: false,
+          isTwoHanded: true,
+        };
+        const findUniqueArgsMock: Prisma.WeaponFindUniqueArgs = {
+          where: { id: existingWeapon.id },
+          include: {
+            variants: true,
+          },
+        };
+
+        prismaMock.weapon.findUnique.mockResolvedValue(existingWeapon);
+
+        const result = await service.findOneWeapon(existingWeapon.id);
+        expect(result).toEqual(existingWeapon);
+        expect(prismaMock.weapon.findUnique).toHaveBeenCalledTimes(1);
+        expect(prismaMock.weapon.findUnique).toHaveBeenCalledWith(
+          findUniqueArgsMock,
+        );
+      });
+
+      it('should throw NotFoundException if weapon does not exist', async () => {
+        const findUniqueArgsMock: Prisma.WeaponFindUniqueArgs = {
+          where: { id: 999 },
+          include: {
+            variants: true,
+          },
+        };
+
+        prismaMock.weapon.findUnique.mockResolvedValue(null);
+
+        await expect(service.findOneWeapon(999)).rejects.toThrow(
+          NotFoundException,
+        );
+        expect(prismaMock.weapon.findUnique).toHaveBeenCalledTimes(1);
+        expect(prismaMock.weapon.findUnique).toHaveBeenCalledWith(
+          findUniqueArgsMock,
+        );
+      });
+    });
+
+    describe('create', () => {
+      it('should create a new weapon', async () => {
+        const createDto: CreateWeaponDto = {
+          name: 'Weapon One',
+          element: 'IMPACT',
+          faction: 'NONE',
+          isRanged: false,
+          isTwoHanded: true,
+        };
+        const createdWeapon: Weapon = {
+          id: 1,
+          name: 'Weapon One',
+          element: 'IMPACT',
+          faction: 'NONE',
+          isRanged: false,
+          isTwoHanded: true,
+        };
+        const createArgsMock: Prisma.WeaponCreateArgs = { data: createDto };
+
+        prismaMock.weapon.create.mockResolvedValue(createdWeapon);
+
+        const result = await service.createWeapon(createDto);
+        expect(result).toEqual(createdWeapon);
+        expect(prismaMock.weapon.create).toHaveBeenCalledTimes(1);
+        expect(prismaMock.weapon.create).toHaveBeenCalledWith(createArgsMock);
+      });
+
+      it('should throw BadRequestException on duplicate name', async () => {
+        const createDto: CreateWeaponDto = {
+          name: 'Weapon One',
+          element: 'IMPACT',
+          faction: 'NONE',
+          isRanged: false,
+          isTwoHanded: true,
+        };
+
+        prismaMock.weapon.create.mockRejectedValue({
+          code: 'P2002',
+          clientVersion: '4.7.1',
+        });
+
+        await expect(service.createWeapon(createDto)).rejects.toThrow(
+          BadRequestException,
+        );
+        expect(prismaMock.weapon.create).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe('update', () => {
+      it('should update an existing weapon', async () => {
+        const existingWeapon: Weapon = {
+          id: 1,
+          name: 'Weapon One',
+          element: 'IMPACT',
+          faction: 'NONE',
+          isRanged: false,
+          isTwoHanded: true,
+        };
+        const updateDto: UpdateWeaponDto = { name: 'Updated Weapon' };
+        const updatedWeapon: Weapon = {
+          id: 1,
+          name: 'Updated Weapon',
+          element: 'IMPACT',
+          faction: 'NONE',
+          isRanged: false,
+          isTwoHanded: true,
+        };
+        const updateArgsMock: Prisma.WeaponUpdateArgs = {
+          where: { id: existingWeapon.id },
+          data: updateDto,
+        };
+
+        prismaMock.weapon.findUnique.mockResolvedValue(existingWeapon);
+        prismaMock.weapon.update.mockResolvedValue(updatedWeapon);
+
+        const result = await service.updateWeapon(1, updateDto);
+        expect(result).toEqual(updatedWeapon);
+        expect(prismaMock.weapon.update).toHaveBeenCalledTimes(1);
+        expect(prismaMock.weapon.update).toHaveBeenCalledWith(updateArgsMock);
+      });
+
+      it('should throw NotFoundException when updating non-existent weapon', async () => {
+        const updateDto = { name: 'Updated Weapon' };
+
+        prismaMock.weapon.update.mockRejectedValue({
+          code: 'P2025',
+          clientVersion: '4.7.1',
+        });
+
+        await expect(service.updateWeapon(999, updateDto)).rejects.toThrow(
+          NotFoundException,
+        );
+        expect(prismaMock.weapon.update).toHaveBeenCalledTimes(0);
+      });
+    });
+
+    describe('remove', () => {
+      it('should delete a weapon without weapons', async () => {
+        const weaponToDelete: Weapon = {
+          id: 1,
+          name: 'Weapon One',
+          element: 'IMPACT',
+          faction: 'NONE',
+          isRanged: false,
+          isTwoHanded: true,
+        };
+        const deleteArgsMock: Prisma.WeaponDeleteArgs = {
+          where: { id: weaponToDelete.id },
+        };
+
+        prismaMock.weapon.findUnique.mockResolvedValue(weaponToDelete);
+        prismaMock.weapon.delete.mockResolvedValue(weaponToDelete);
+
+        const result = await service.removeWeapon(1);
+        expect(result).toEqual(weaponToDelete);
+        expect(prismaMock.weapon.findUnique).toHaveBeenCalledTimes(1);
+        expect(prismaMock.weapon.delete).toHaveBeenCalledTimes(1);
+        expect(prismaMock.weapon.delete).toHaveBeenCalledWith(deleteArgsMock);
+      });
+
+      it('should throw NotFoundException when deleting non-existent weapon', async () => {
+        prismaMock.weapon.findUnique.mockResolvedValue(null);
+
+        await expect(service.removeWeapon(999)).rejects.toThrow(
+          NotFoundException,
+        );
+        expect(prismaMock.weapon.findUnique).toHaveBeenCalledTimes(1);
+        expect(prismaMock.weapon.delete).not.toHaveBeenCalled();
+      });
+    });
+  });
 
   describe('Armor Variants', () => {});
 
