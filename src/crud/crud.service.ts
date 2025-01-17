80ea242957f026d8faee271d5c14ff2671a64fc1
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-16 23:00:43 -0500

    Delete unused files

--- src/crud/crud.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
-
-@Injectable()
-export class CrudService {
-  constructor(private prisma: PrismaService) {}
-}
