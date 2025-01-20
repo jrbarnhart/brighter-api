e853a97120785030d2f90932d81936a6a6223130
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-20 17:54:09 -0500

    Update controller for swagger

--- src/items/miscItems/miscItems.controller.ts
+++ src/items/miscItems/miscItems.controller.ts
@@ -7,47 +7,74 @@ import {
   Param,
   Delete,
   UseGuards,
-  UsePipes,
   ParseIntPipe,
 } from '@nestjs/common';
 import { AuthGuard } from 'src/auth/auth.guard';
-import { ZodValidationPipe } from 'src/validation/zodValidation.pipe';
 import { MiscItemsService } from './miscItems.service';
+import { CreateMiscItemDto } from './dto/create-miscItem.dto';
+import { UpdateMiscItemDto } from './dto/update-miscItem.dto';
 import {
-  CreateMiscItemDto,
-  createMiscItemSchema,
-} from './dto/create-miscItem.dto';
-import {
-  UpdateMiscItemDto,
-  updateMiscItemSchema,
-} from './dto/update-miscItem.dto';
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
-@Controller('items/misc')
+@Controller('items/miscItems')
 export class MiscItemsController {
   constructor(private readonly miscItemsService: MiscItemsService) {}
 
   @Post()
   @UseGuards(AuthGuard)
-  @UsePipes(new ZodValidationPipe(createMiscItemSchema))
+  @ApiOperation({
+    summary: 'Create miscItem',
+    description: 'This creates a new miscItem record',
+  })
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ description: 'MiscItem created' })
+  @ApiBadRequestResponse({ description: 'Bad request, invalid body data' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized access' })
   create(@Body() createMiscItemDto: CreateMiscItemDto) {
     return this.miscItemsService.create(createMiscItemDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Get all miscItem',
+    description: 'This gets all miscItem records',
+  })
+  @ApiOkResponse({ description: 'Found all miscItem records' })
   findAll() {
     return this.miscItemsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Get miscItem by id',
+    description: 'This gets one miscItem by its id',
+  })
+  @ApiOkResponse({ description: 'Found miscItem record' })
+  @ApiNotFoundResponse({ description: 'MiscItem not found' })
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.miscItemsService.findOne(id);
   }
 
   @Patch(':id')
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: 'Update miscItem',
+    description: 'This updates an miscItem record by id',
+  })
+  @ApiBearerAuth()
+  @ApiNotFoundResponse({ description: 'MiscItem not found' })
+  @ApiBadRequestResponse({ description: 'Bad request, invalid body data' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized access' })
   update(
     @Param('id', ParseIntPipe) id: number,
-    @Body(new ZodValidationPipe(updateMiscItemSchema))
+    @Body()
     updateMiscItemDto: UpdateMiscItemDto,
   ) {
     return this.miscItemsService.update(id, updateMiscItemDto);
@@ -55,6 +82,14 @@ export class MiscItemsController {
 
   @Delete(':id')
   @UseGuards(AuthGuard)
+  @ApiOperation({
+    summary: 'Delete miscItem',
+    description: 'This deletes an miscItem record by id',
+  })
+  @ApiBearerAuth()
+  @ApiOkResponse({ description: 'MiscItem was deleted' })
+  @ApiNotFoundResponse({ description: 'MiscItem not found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized access' })
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.miscItemsService.remove(id);
   }
