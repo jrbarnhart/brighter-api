2bf4b627eddea67d800fb2ce3dae50a68cc6b326
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-20 14:45:25 -0500

    Add class dto for sign in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,6 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  HttpStatus,
-  Post,
-  UsePipes,
-} from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { SignInDto, signInSchema } from './dto/signIn.dto';
-import { ZodValidationPipe } from 'src/validation/zodValidation.pipe';
+import { SignInDto } from './dto/signIn.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -16,7 +8,6 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  @UsePipes(new ZodValidationPipe(signInSchema))
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
--- src/auth/dto/signIn.dto.ts
+++ src/auth/dto/signIn.dto.ts
@@ -1,8 +1,19 @@
-import { z } from 'zod';
+// import { z } from 'zod';
 
-export const signInSchema = z.object({
-  username: z.string(),
-  password: z.string(),
-});
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export type SignInDto = z.infer<typeof signInSchema>;
+// export const signInSchema = z.object({
+//   username: z.string(),
+//   password: z.string(),
+// });
+
+// export type SignInDto = z.infer<typeof signInSchema>;
+export class SignInDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
