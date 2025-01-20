6b59d307911eb5218a7ea86f1f766a5c74a571bf
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-20 13:21:54 -0500

    Implement sign in dto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto, signInSchema } from './dto/signIn.dto';
+import { ZodValidationPipe } from 'src/validation/zodValidation.pipe';
 
 @Controller('auth')
 export class AuthController {
@@ -7,7 +16,8 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
+  @UsePipes(new ZodValidationPipe(signInSchema))
+  signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto.username, signInDto.password);
   }
 }
