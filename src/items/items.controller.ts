c0bf55a4b6a912bef6997885336cfb60db29d35f
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-20 18:47:23 -0500

    Add swagger decorators

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { ItemsService } from './items.service';
+import { ApiBadRequestResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Invalid access',
+    description: 'This warns users and shows an example valid items query',
+  })
+  @ApiBadRequestResponse({ description: 'Cannot directly query items warning' })
   find() {
     this.itemsService.find();
   }
