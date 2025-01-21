05ffa548439d1676bba8a9c7d9c5a76431060f8c
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-20 20:03:59 -0500

    Fix path

--- generate/templates/controller.template.ts
+++ generate/templates/controller.template.ts
@@ -1,60 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const controllerTemplateOld = `
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  UseGuards,
-  UsePipes,
-  ParseIntPipe,
-} from '@nestjs/common';
-import { AuthGuard } from 'src/auth/auth.guard';
-import { ZodValidationPipe } from 'src/validation/zodValidation.pipe';
-import { __PASCAL_PLURAL__Service } from './__CAMEL_PLURAL__.service';
-import { Create__PASCAL__Dto, create__PASCAL__Schema } from './dto/create-__CAMEL__.dto';
-import { Update__PASCAL__Dto, update__PASCAL__Schema } from './dto/update-__CAMEL__.dto';
-
-@Controller('__PLURAL__')
-export class __PASCAL_PLURAL__Controller {
-  constructor(private readonly __CAMEL_PLURAL__Service: __PASCAL_PLURAL__Service) {}
-
-  @Post()
-  @UseGuards(AuthGuard)
-  @UsePipes(new ZodValidationPipe(create__PASCAL__Schema))
-  create(@Body() create__PASCAL__Dto: Create__PASCAL__Dto) {
-    return this.__CAMEL_PLURAL__Service.create(create__PASCAL__Dto);
-  }
-
-  @Get()
-  findAll() {
-    return this.__CAMEL_PLURAL__Service.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.__CAMEL_PLURAL__Service.findOne(id);
-  }
-
-  @Patch(':id')
-  @UseGuards(AuthGuard)
-  update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body(new ZodValidationPipe(update__PASCAL__Schema)) update__PASCAL__Dto: Update__PASCAL__Dto,
-  ) {
-    return this.__CAMEL_PLURAL__Service.update(id, update__PASCAL__Dto);
-  }
-
-  @Delete(':id')
-  @UseGuards(AuthGuard)
-  remove(@Param('id', ParseIntPipe) id: number) {
-    return this.__CAMEL_PLURAL__Service.remove(id);
-  }
-}`;
-
 export const controllerTemplate = `
 import {
   Controller,
@@ -81,7 +24,7 @@ import {
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 
-@Controller('items/__CAMEL_PLURAL__')
+@Controller('__CAMEL_PLURAL__')
 export class __PASCAL_PLURAL__Controller {
   constructor(private readonly __CAMEL_PLURAL__Service: __PASCAL_PLURAL__Service) {}
 
