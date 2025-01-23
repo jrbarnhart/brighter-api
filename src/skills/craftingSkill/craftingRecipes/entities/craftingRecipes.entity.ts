a5f8597c91fb4ec69414951b0c0deb59be0b9ce0
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-23 17:16:20 -0500

    Add crafting recipe entity with comment placeholders

--- src/skills/craftingSkill/craftingRecipes/entities/craftingRecipes.entity.ts
+++ src/skills/craftingSkill/craftingRecipes/entities/craftingRecipes.entity.ts
@@ -1,1 +1,36 @@
-export class CraftingRecipeEntity {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+import { ArmorVariantEntity } from 'src/items/armors/armorVariants/entities/armorVariants.entity';
+
+export class CraftingRecipeEntity {
+  @IsNotEmpty()
+  @IsNumber()
+  @IsInt()
+  @IsPositive()
+  id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(256)
+  name: string;
+
+  //requirement?: CraftingSkillRequirementEntity
+
+  @IsNumber()
+  @IsInt()
+  @IsPositive()
+  requirementId?: number | null;
+
+  //inputResourceVariants?: ResourceVariantEntity[]
+  //inputItems?: MiscItemEntity[]
+
+  //outputConsumableVariant?: ConsumableVariantEntity
+  //outputWeaponVariant?: WeaponVariantEntity
+  outputArmorVariant?: ArmorVariantEntity;
+}
