7c1028846bb2450588cae5870704da29de3360df
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-23 14:27:55 -0500

    Add basic structure for room entity

---
+++ src/rooms/entities/rooms.entity.ts
@@ -0,0 +1,64 @@
+import { BankType } from '@prisma/client';
+import {
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsNumberString,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { RegionEntity } from 'src/regions/entities/regions.entity';
+
+export class RoomEntity {
+  @IsNotEmpty()
+  @IsNumberString()
+  @IsInt()
+  @IsPositive()
+  id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @MaxLength(256)
+  name: string;
+
+  @IsNotEmpty()
+  region: RegionEntity;
+
+  @IsNotEmpty()
+  @IsNumberString()
+  @IsInt()
+  @IsPositive()
+  regionId: number;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  portal: boolean;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  obelisk: boolean;
+
+  //   craftingSKills: CraftingSkillEntity[]
+
+  //   monsters: MonsterEntity[]
+
+  //   npcs: NpcEntity[]
+
+  //   resources: ResourceEntity[]
+
+  //   questSteps: QuestStepEntity
+
+  @IsArray()
+  @IsEnum(BankType, { each: true })
+  @ApiProperty({
+    description: 'The array of types of bank in this room',
+    isArray: true,
+    enum: BankType,
+    type: String,
+  })
+  banks?: BankType[];
+}
