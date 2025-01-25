6d0f00892c8919300b52c52c3ac052941459c168
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-25 12:44:03 -0500

    The only thing you actually update directly is name

--- src/regions/dto/update-region.dto.ts
+++ src/regions/dto/update-region.dto.ts
@@ -1,35 +1,8 @@
-import { IsOptional, IsString, MaxLength, IsArray } from 'class-validator';
-import { ApiPropertyOptional } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
-import { ConnectDto } from 'src/generalDto/prisma-connect.dto';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class UpdateRegionDto {
   @IsString()
   @IsOptional()
   @MaxLength(256)
   name?: string;
-
-  @ApiPropertyOptional({ type: [ConnectDto] })
-  @IsOptional()
-  @IsArray()
-  @Type(() => ConnectDto)
-  rooms?: { connect: { id: number }[] };
-
-  @ApiPropertyOptional({ type: [ConnectDto] })
-  @IsOptional()
-  @IsArray()
-  @Type(() => ConnectDto)
-  combatSkills?: { connect: { id: number }[] };
-
-  @ApiPropertyOptional({ type: [ConnectDto] })
-  @IsOptional()
-  @IsArray()
-  @Type(() => ConnectDto)
-  gatheringSkills?: { connect: { id: number }[] };
-
-  @ApiPropertyOptional({ type: [ConnectDto] })
-  @IsOptional()
-  @IsArray()
-  @Type(() => ConnectDto)
-  craftingSkills?: { connect: { id: number }[] };
 }
