b6d537bbf602f4624b0aa51a652e749ad993f03e
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-01-25 12:43:34 -0500

    Add comment showing use

--- src/generalDto/prisma-connect.dto.ts
+++ src/generalDto/prisma-connect.dto.ts
@@ -4,3 +4,13 @@ export class ConnectDto {
   @ApiPropertyOptional()
   id: number;
 }
+
+/*
+Use it like this:
+
+  @ApiPropertyOptional({ type: [ConnectDto] })
+  @IsOptional()
+  @IsArray()
+  @Type(() => ConnectDto)
+  craftingSkills?: { connect: { id: number }[] };
+*/
