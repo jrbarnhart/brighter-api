afd096645863080be3bb219c5ed028488df1d177
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-02 18:48:25 -0500

    Fix optional dto props

--- src/skills/gatheringSkill/gatheringSkillRequirement/dto/update-gatheringSkillRequirement.dto.ts
+++ src/skills/gatheringSkill/gatheringSkillRequirement/dto/update-gatheringSkillRequirement.dto.ts
@@ -1,33 +1,33 @@
 import {
   IsInt,
-  IsNotEmpty,
   IsNumber,
+  IsOptional,
   IsPositive,
   IsString,
   MaxLength,
 } from 'class-validator';
 
 export class UpdateGatheringSkillRequirementDto {
-  @IsNotEmpty()
   @IsNumber()
   @IsInt()
   @IsPositive()
+  @IsOptional()
   skillId?: number;
 
-  @IsNotEmpty()
   @IsNumber()
   @IsInt()
   @IsPositive()
+  @IsOptional()
   unlockLevel?: number;
 
-  @IsNotEmpty()
   @IsString()
   @MaxLength(400)
+  @IsOptional()
   description?: string;
 
-  @IsNotEmpty()
   @IsNumber()
   @IsInt()
   @IsPositive()
+  @IsOptional()
   resourceVariantId?: number;
 }
