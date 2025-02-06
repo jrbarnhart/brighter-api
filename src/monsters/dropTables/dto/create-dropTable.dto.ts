d21829b0f884e1c090f50ae9d5470fac4469ec14
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-06 17:30:09 -0500

    Mark props as optional

--- src/monsters/dropTables/dto/create-dropTable.dto.ts
+++ src/monsters/dropTables/dto/create-dropTable.dto.ts
@@ -3,6 +3,7 @@ import {
   IsInt,
   IsNotEmpty,
   IsNumber,
+  IsOptional,
   IsPositive,
 } from 'class-validator';
 
@@ -17,34 +18,40 @@ export class CreateDropTableDto {
   @IsNumber({}, { each: true })
   @IsInt({ each: true })
   @IsPositive({ each: true })
+  @IsOptional()
   resourceVariantIds?: number[];
 
   @IsArray()
   @IsNumber({}, { each: true })
   @IsInt({ each: true })
   @IsPositive({ each: true })
+  @IsOptional()
   weaponVariantIds?: number[];
 
   @IsArray()
   @IsNumber({}, { each: true })
   @IsInt({ each: true })
   @IsPositive({ each: true })
+  @IsOptional()
   armorVariantIds?: number[];
 
   @IsArray()
   @IsNumber({}, { each: true })
   @IsInt({ each: true })
   @IsPositive({ each: true })
+  @IsOptional()
   consumableVariantIds?: number[];
 
   @IsArray()
   @IsNumber({}, { each: true })
   @IsInt({ each: true })
   @IsPositive({ each: true })
+  @IsOptional()
   miscItemIds?: number[];
 
   @IsNumber()
   @IsInt()
   @IsPositive()
+  @IsOptional()
   currency?: number;
 }
--- src/monsters/dropTables/dto/update-dropTable.dto.ts
+++ src/monsters/dropTables/dto/update-dropTable.dto.ts
@@ -1,50 +1,56 @@
 import {
   IsArray,
   IsInt,
-  IsNotEmpty,
   IsNumber,
+  IsOptional,
   IsPositive,
 } from 'class-validator';
 
 export class UpdateDropTableDto {
-  @IsNotEmpty()
   @IsNumber()
   @IsInt()
   @IsPositive()
+  @IsOptional()
   monsterVariantId?: number;
 
   @IsArray()
   @IsNumber({}, { each: true })
   @IsInt({ each: true })
   @IsPositive({ each: true })
+  @IsOptional()
   resourceVariantIds?: number[];
 
   @IsArray()
   @IsNumber({}, { each: true })
   @IsInt({ each: true })
   @IsPositive({ each: true })
+  @IsOptional()
   weaponVariantIds?: number[];
 
   @IsArray()
   @IsNumber({}, { each: true })
   @IsInt({ each: true })
   @IsPositive({ each: true })
+  @IsOptional()
   armorVariantIds?: number[];
 
   @IsArray()
   @IsNumber({}, { each: true })
   @IsInt({ each: true })
   @IsPositive({ each: true })
+  @IsOptional()
   consumableVariantIds?: number[];
 
   @IsArray()
   @IsNumber({}, { each: true })
   @IsInt({ each: true })
   @IsPositive({ each: true })
+  @IsOptional()
   miscItemIds?: number[];
 
   @IsNumber()
   @IsInt()
   @IsPositive()
+  @IsOptional()
   currency?: number;
 }
