442982f8a3ed8719c624166e493d859b83123426
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-15 20:34:03 -0500

    Remove incorrect unique constraints from schema

---
+++ prisma/migrations/20250216013342_remove_incorrect_uniques/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "ArmorVariant_armorId_key";
+
+-- DropIndex
+DROP INDEX "ConsumableVariant_consumableId_key";
+
+-- DropIndex
+DROP INDEX "QuestStep_questId_key";
+
+-- DropIndex
+DROP INDEX "ResourceVariant_resourceId_key";
