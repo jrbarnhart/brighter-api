be6f475c58eb6130d2311dd02cf0d41495ee1692
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-18 16:55:53 -0500

    Return metadata on root request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getMetaData() {
+    return this.appService.getMetadata();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,40 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getMetadata() {
+    return {
+      name: 'Brighter Shores API',
+      version: '1.0.0',
+      description:
+        'An API for the Brighter Shores MMO, providing data on regions, rooms, skills, monsters, items, quests, and npcs.',
+      endpoints: {
+        stats: '/stats',
+        regions: '/regions',
+        rooms: '/rooms',
+        combatSkills: '/skills/combat',
+        combatSkillRequirements: '/skills/combat/requirements',
+        gatheringSkills: '/skills/gathering',
+        gatheringSkillRequirements: '/skills/gathering/requirements',
+        craftingSkills: '/skills/crafting',
+        craftingSkillRequirements: '/skills/crafting/requirements',
+        craftingRecipes: '/skills/crafting/recipes',
+        resources: '/items/resources',
+        resourceVariants: 'items/resources/variants',
+        consumables: '/items/consumables',
+        consumableVariants: 'items/consumables/variants',
+        weapons: '/items/weapons',
+        weaponVariants: 'items/weapons/variants',
+        armors: '/items/armors',
+        armorVariants: '/items/armors/variants',
+        miscItems: '/items/misc',
+        monsters: '/monsters',
+        monsterVariants: '/monsters/variants',
+        dropTables: '/monsters/drop-tables',
+        npcs: '/npcs',
+        vendors: '/npcs/vendors',
+        quests: '/quests',
+        questSteps: '/quests/steps',
+      },
+    };
   }
 }
