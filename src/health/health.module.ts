2017a1440ac2378e08ad3f30f8c08b787021523c
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-26 18:07:59 -0500

    Provide logger

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { TerminusModule } from '@nestjs/terminus';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   imports: [TerminusModule],
   controllers: [HealthController],
+  providers: [PrismaService, Logger],
 })
 export class HealthModule {}
