047c697a06d02e8f568e9eb536e4870b8f87e883
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-02-26 19:10:39 -0500

    Simplify health check and let terminus handle logging

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  PrismaHealthIndicator,
+} from '@nestjs/terminus';
+import { PrismaService } from 'src/prisma.service';
 
 @Controller('health')
-export class HealthController {}
+export class HealthController {
+  private wasUnhealthy = false;
+
+  constructor(
+    private health: HealthCheckService,
+    private prismaHealth: PrismaHealthIndicator,
+    private prismaService: PrismaService,
+    private logger: Logger,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.prismaHealth.pingCheck('database', this.prismaService),
+    ]);
+  }
+}
