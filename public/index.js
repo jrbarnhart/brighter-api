4506c8ac1fa89d844d374f080c5d1535834e482b
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-04 20:01:21 -0500

    Style demo section and add placeholder data

--- public/index.js
+++ public/index.js
@@ -1,3 +1,51 @@
+const placeholderResponseData = `{
+    "status": 200,
+    "success": true,
+    "data": [
+        {
+            "id": 19,
+            "name": "Sparring Guard",
+            "skillId": 4,
+            "regionId": 31,
+            "passive": false,
+            "attackElement": "IMPACT",
+            "immuneElement": "NONE",
+            "vulnerableElement": "NONE",
+            "rooms": [],
+            "skill": {
+                "id": 4,
+                "name": "Guard",
+                "regionId": 31
+            },
+            "variants": [],
+            "region": {
+                "id": 31,
+                "name": "Hopeport"
+            }
+        },
+        {
+            "id": 20,
+            "name": "Deathcrow",
+            "skillId": 4,
+            "regionId": 31,
+            "passive": false,
+            "attackElement": "NECROMAE",
+            "immuneElement": "NECROMAE",
+            "vulnerableElement": "CRYONAE",
+            "rooms": [],
+            "skill": {
+                "id": 4,
+                "name": "Guard",
+                "regionId": 31
+            },
+            "variants": [],
+            "region": {
+                "id": 31,
+                "name": "Hopeport"
+            }
+        }
+    }`;
+
 function main() {
   // Refs to elements
   const devForm = document.getElementById('dev-form');
@@ -35,6 +83,15 @@ function main() {
       e.preventDefault();
       handleProdSubmit();
     });
+  }
+
+  // Set the default value and response data
+  if (demoInput) {
+    demoInput.setAttribute('value', '/monsters');
+  }
+
+  if (returnedDataP) {
+    returnedDataP.innerText = placeholderResponseData;
   }
 }
 
