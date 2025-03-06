b03dd2fbe905cbc6590afb8e1743e03947f65814
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-06 01:11:50 -0500

    Dont document index

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Get, Controller, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private appService: AppService) {}
 
+  @ApiExcludeEndpoint(true)
   @Get()
   @Render('index')
   root() {
