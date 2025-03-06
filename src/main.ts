9be76468841f803ff1c2d0d32ad636c9b1990e78
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-06 01:12:05 -0500

    Add dark style to swagger docs

--- src/main.ts
+++ src/main.ts
@@ -29,19 +29,96 @@ async function bootstrap() {
   app.setViewEngine('hbs');
 
   // Swagger documentation
-  const config = new DocumentBuilder()
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Brighter API')
     .setDescription('Returns data about the MMORPG Brighter Shores')
     .setVersion('1.0')
     .build();
 
-  const customOptions: SwaggerCustomOptions = {
+  const swaggerCustomOptions: SwaggerCustomOptions = {
     jsonDocumentUrl: 'api-json',
+    customCss: `
+      :root {
+      --bg-color: #0f1115;
+      --primary-color: #f0f2f5;
+      --secondary-color:rgb(184, 199, 219);
+      --accent: #3b82f6;
+      --get-background: #dcedff;
+      --post-background: #defaee;
+      --delete-background: #f7e0e0;
+      --model-background: #333333;
+      --prop-type-color:rgb(158, 195, 255);
+    }
+
+    body {
+      background-color: var(--bg-color);
+      color-scheme: dark;
+    }
+
+    .swagger-ui .title {
+      color: var(--accent) !important;
+    }
+
+    .swagger-ui .model-title {
+      color: var(--primary-color);
+    }
+
+    .topbar {
+      display: none;
+    }
+
+    .swagger-ui .info li, .swagger-ui .info p, .swagger-ui .info table {
+      color: var(--secondary-color);
+    }
+
+    .swagger-ui .opblock.opblock-get {
+      background-color: var(--get-background);
+    }
+
+    .swagger-ui .opblock.opblock-post,
+    .swagger-ui .opblock.opblock-patch {
+      background-color: var(--post-background);
+    }
+
+    .swagger-ui .opblock.opblock-delete {
+      background-color: var(--delete-background);
+    }
+
+    .swagger-ui .opblock-tag.no-desc span {
+      color: var(--primary-color);
+    }
+
+    .swagger-ui section.models h4 span {
+      color: var(--primary-color);
+    }
+
+    .swagger-ui .model {
+      color: var(--primary-color);
+      }
+      
+    .swagger-ui .model-box {    
+      background-color: var(--model-background)
+    }
+
+    .swagger-ui .model-toggle:after {
+      filter: invert(1);
+    }
+
+    .swagger-ui .prop-type {
+      color: var(--prop-type-color);
+    }
+
+    .swagger-ui .model .property.primitive,
+    .swagger-ui .model .renderedMarkdown p {
+      color: var(--secondary-color);
+    }
+    `,
   };
 
   await SwaggerModule.loadPluginMetadata(metadata);
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory, customOptions);
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory, swaggerCustomOptions);
 
   // Required for health check
   app.enableShutdownHooks();
