eab86cafa2d35c45aa1bcb76544847d2f6009575
Joshua Barnhart <151244381+jrbarnhart@users.noreply.github.com>
2025-03-31 18:53:41 -0400

    Add base for reading and returning log files

--- src/logs/logs.service.ts
+++ src/logs/logs.service.ts
@@ -1,8 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import fs from 'fs';
+import zlib from 'zlib';
+import readline from 'readline';
 
 @Injectable()
 export class LogsService {
   getCombinedLogs() {
-    return { combinedErrors: [], combinedLogs: [] };
+    const combinedLogs: Record<any, any>[] = [];
+    const combinedErrors: Record<any, any>[] = [];
+
+    // For each .log or .gz file in /logs
+    const dirPath = __dirname + '/logs';
+    fs.readdir(dirPath, (err, files) => {
+      if (err) {
+        // Handle error
+      }
+
+      files.forEach(async (file) => {
+        // File type
+        const isGzip = file.endsWith('.gz');
+        const isLog = file.endsWith('.log');
+        // Error or log file
+        const isError = file.startsWith('error');
+
+        // Create readline interface
+        const lineReader = isGzip
+          ? readline.createInterface({
+              input: fs.createReadStream(file).pipe(zlib.createGunzip()),
+            })
+          : isLog
+            ? readline.createInterface({ input: fs.createReadStream(file) })
+            : null;
+
+        // For each line in the interface add it to combinedErrors or combinedLogs
+        if (lineReader) {
+          for await (const line of lineReader) {
+            if (isError) {
+              combinedErrors.push(JSON.parse(line));
+            } else {
+              combinedLogs.push(JSON.parse(line));
+            }
+          }
+        }
+      });
+    });
+
+    return { combinedLogs, combinedErrors };
   }
 }
